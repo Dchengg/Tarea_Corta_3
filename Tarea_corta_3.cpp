c84d1d087f036ba8079044e30403baf8e79cb143
Juanca0399 <juancaruizs@github.com>
2017-11-01 18:39:44 -0600

    El metodo de borrar esta a medio camino

--- Tarea_corta_3.cpp
+++ Tarea_corta_3.cpp
@@ -67,6 +67,7 @@ public:
     void indexar();
     void insertarCliente();
     void limpiar();
+    void EliminarCliente (int cedula);
 };
 
 class lista {
@@ -97,7 +98,6 @@ lista::~lista()
 	}
 	actual = NULL;
 }
-
 
 void NodoBinario::InsertaBinario(int num,string str)
 {
@@ -173,22 +173,21 @@ string getSegmento(string str, int pos){
   return resultado;
 }
 
-
 void Binario::insertarIndices(){
   std::ifstream Archivo("Indices.txt");
   string str;
   string segmento;
   int llave;
-
   while (std::getline(Archivo, segmento))
   {
-    cout<<segmento<<endl;
+    // cout<<segmento<<endl;
     llave = stoi(getSegmento(segmento,2));
     str = getSegmento(segmento,1);
     InsertaNodo(llave,str);
 
   }
 }
+
 void lista::Mostrar()
 {
    pnodo aux=primero;
@@ -201,25 +200,22 @@ void lista::Mostrar()
      cout<<aux->valor<<"->";
      cout<<endl;
 }
-void lista::InsertarFinal(string v)
-{
+
+void lista::InsertarFinal(string v){
 	if (ListaVacia()){
 		primero = new nodo(v);
   }
-	else
-	{
+	else{
 		pnodo aux = primero;
 		while (aux->siguiente != NULL)
 			aux = aux->siguiente;
 		aux->siguiente = new nodo(v);
 	}
 }
-void lista::BorrarInicio()
-{
-	if (ListaVacia()) {
-		cout << "No hay elementos en la lista:" << endl;
 
-	}
+void lista::BorrarInicio(){
+	if (ListaVacia())
+		cout << "No hay elementos en la lista:" << endl;
 	else {
 		if (primero->siguiente == NULL) {
 			primero = NULL;
@@ -233,13 +229,13 @@ void lista::BorrarInicio()
 	}
 }
 
-
 void lista::limpiar(){
 	while (!ListaVacia())
 	{
 		BorrarInicio();
 	}
 }
+
 void lista::CargarMemoria(int indice){
   int pos;
   int times;
@@ -257,9 +253,11 @@ void lista::CargarMemoria(int indice){
     cont++;
   }
 }
+
 void Binario::limpiar(){
   raiz = NULL;
 }
+
 void Binario::insertarCliente(){
   std::ofstream Archivo("Clientes.txt",fstream::app);
   string cliente;
@@ -286,40 +284,142 @@ void CrearIndices(){
   std::ifstream Archivo(archivo);
 	std::string str;
   while (std::getline(Archivo, str)) {
-    expresion = str;
-    cont++;
-    expresion = to_string(cont)+","+expresion;
-    arch1 << expresion << std::endl;
-
+    if (str.back() != '1'){
+      expresion = str;
+      cont++;
+      expresion = to_string(cont)+","+expresion;
+      arch1 << expresion << std::endl;
+    }
   }
 }
+
 void Binario::indexar(){
   CrearIndices();
   limpiar();
   insertarIndices();
-  pNodoBinario aux = raiz;
-  InordenR(aux);
 }
+
+/*void Binario::Borrar(NodoBinario* D){
+  NodoBinario *q;
+  if (D->Hder != NULL)
+    Borrar(D->Hder);
+  else{
+    q->valor = D->valor;
+    q = D;
+    D = D->Hizq;
+  }
+}
+
+void Binario::BorrarBalanceado(NodoBinario *raiz, int x){
+  NodoBinario *q;
+  cout << "raiz:  " << raiz->valor << endl;
+  if (raiz != NULL){
+    if (x < raiz->valor){
+      cout << "entro" << endl;
+      BorrarBalanceado(raiz->Hizq, x);
+    }
+    else{
+      if (x > raiz->valor){
+        cout << "entro 2" << endl;
+        BorrarBalanceado(raiz->Hder, x);
+      }
+      else
+        q = raiz;
+    }
+    if (q->Hder == NULL){
+      cout << "entro 3" << endl;
+      raiz = q->Hizq;
+    }
+    else{
+      if (q->Hizq == NULL){
+        raiz = q->Hder;
+      }
+    }
+  }
+  else{
+    Borrar(q->Hizq);
+  }
+}*/
+
+
+void AgregarUno(string cedula){
+  string str, str2;
+  ifstream original ( "Clientes.txt" );
+  ofstream copia ( "Clientes2.txt" );
+  while (!original.eof()){
+      getline(original,str);
+      str2 = str;
+      if (getSegmento(str,1) == cedula)
+        str2 = str + ",1";
+      copia << str2 << endl;
+  }
+  original.close();
+  copia.close();
+  remove("Clientes.txt");
+  rename("Clientes2.txt","Clientes.txt");
+  ifstream original2 ( "Clientes.txt" );
+  ofstream copia2 ( "Clientes2.txt" );
+  string linea;
+  while ( getline( original2, linea ) ) {
+    if ( ! linea.empty() ) {
+        copia2 << linea << '\n';
+    }
+  }
+  original2.close();
+  copia2.close();
+  remove("Clientes.txt");
+  rename("Clientes2.txt","Clientes.txt");
+}
+
+void Binario::EliminarCliente (int cedula){
+  //if (BorrarBalanceado(raiz,cedula)){
+  string cedulaString = to_string(cedula);
+  AgregarUno(cedulaString);
+  //}
+  //else{
+    //cout << "La cédula que digito no existe" << endl;
+  //}
+}
+
 int main(){
   Binario Arbol;
   lista Mem;
   int opcion;
   CrearIndices();
   Arbol.insertarIndices();
-  cout<<"Bievenido"<<endl;
-  cout<<"Digite el numero de la accion que desea realizar :"<<endl;
-  cout<<"1.Indexar"<<endl;
-  cout<<"2.Agregar un cliente"<<endl;
-  cout<<"3.Eliminar un cliente"<<endl;
-  cin>>opcion;
-  if(opcion == 1){
-    Arbol.indexar();
-  }else if(opcion == 2){
-    Arbol.insertarCliente();
-  }else if(opcion == 3){
-
-  }else{
-    cout<<"Por favor digite una opcion valida"<<endl;
+  bool seguirTrabajando = true;
+  while (seguirTrabajando){
+    cout << "Bievenido" << endl;
+    cout << "Digite el numero de la accion que desea realizar :" << endl;
+    cout << "    1. Indexar" << endl;
+    cout << "    2. Agregar un cliente" << endl;
+    cout << "    3. Eliminar un cliente" << endl;
+    cout << "    99. Mostrar recorridos" << endl;
+    cout << "    100. Salir" << endl;
+    cin >> opcion;
+    if(opcion == 1)
+      Arbol.indexar();
+    else if(opcion == 2)
+      Arbol.insertarCliente();
+    else if(opcion == 3){
+      int clienteAEliminar;
+      cout << endl << "Digite la cedula del cliente que desea eliminar: ";
+      cin >> clienteAEliminar;
+      Arbol.EliminarCliente(clienteAEliminar);
+      Arbol.indexar();
+    }
+    else if(opcion == 99){
+      cout << "Inorden: " << endl;
+      InordenR(Arbol.raiz);
+      cout << endl << endl << "Preorden: " << endl;
+      PreordenR(Arbol.raiz);
+      cout << endl << endl << "Postorden: " << endl;
+      PostordenR(Arbol.raiz);
+    }
+    else if(opcion == 100)
+      seguirTrabajando = false;
+    else
+      cout<<"Por favor digite una opcion valida"<<endl;
   }
   cin.get();
   return 0;
